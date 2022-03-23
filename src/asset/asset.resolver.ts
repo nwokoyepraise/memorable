2cf57013816d1091b26377a18073644099b064cf
nwokoyepraise <nwokoyepraise@gmail.com>
2022-03-23 05:06:49 +0100

    implement get average functionality

--- src/asset/asset.resolver.ts
+++ src/asset/asset.resolver.ts
@@ -1,37 +1,50 @@
-import { AssetModel } from "./asset.model";
-import { AssetService } from "./asset.service";
-import { Resolver, Mutation, Args, Query, ResolveField, Parent} from "@nestjs/graphql";
-import { Inject } from "@nestjs/common";
-import { CreateAssetDto } from "./dto/create_asset.dto";
-import { AddScoresDto } from "./dto/add_scores.dto";
+import { AssetModel } from './asset.model';
+import { AssetService } from './asset.service';
+import {
+  Resolver,
+  Mutation,
+  Args,
+  Query,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
+import { Inject } from '@nestjs/common';
+import { CreateAssetDto } from './dto/create_asset.dto';
+import { AddScoresDto } from './dto/add_scores.dto';
+import { AverageScoreDto } from './dto/average_score.dto';
 
-@Resolver(of => AssetModel)
+@Resolver((of) => AssetModel)
 export class AssetResolver {
-    constructor(
-        @Inject(AssetService) private assetService: AssetService
-    ) {}
+  constructor(@Inject(AssetService) private assetService: AssetService) {}
 
-    @Query(returns => AssetModel)
-    async asset(@Args('id') id: string): Promise<AssetModel>{
-        return await this.assetService.findOne(id);
-    }
+  @Query((returns) => AssetModel)
+  async asset(@Args('id') id: string): Promise<AssetModel> {
+    return await this.assetService.findOne(id);
+  }
 
-    @Query(()=> String)
-    hello(): string {
-        return 'Memorable rocks!';
-    }
+  @Query(() => String)
+  hello(): string {
+    return 'Memorable rocks!';
+  }
 
-    @Mutation(returns => AssetModel)
-    async createAsset (
-        @Args('asset') asset: CreateAssetDto
-    ): Promise<AssetModel> {
-        return await this.assetService.createOne(asset);
-    }
+  @Mutation((returns) => AssetModel)
+  async createAsset(@Args('asset') asset: CreateAssetDto): Promise<AssetModel> {
+    return await this.assetService.createOne(asset);
+  }
 
-    @Mutation(returns => AssetModel)
-    async addScores (
-        @Args('scores') scores:  AddScoresDto
-    , @Args('id') id: string): Promise<AssetModel> {
-        return await this.assetService.addScores(id, scores);
-    }
+  @Mutation((returns) => AssetModel)
+  async addScores(
+    @Args('scores') scores: AddScoresDto,
+    @Args('id') id: string,
+  ): Promise<AssetModel> {
+    return await this.assetService.addScores(id, scores);
+  }
+
+  @Query((returns) => AverageScoreDto)
+  async getAverage(
+    @Args('asset_type') asset_type: string,
+    @Args('score_type') score_type: string
+  ): Promise<AverageScoreDto> {
+    return await this.assetService.getAverageScore(asset_type, score_type);
+  }
 }
--- src/asset/asset.service.ts
+++ src/asset/asset.service.ts
@@ -5,6 +5,8 @@ import { AssetModel } from './asset.model';
 import { CreateAssetDto } from './dto/create_asset.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { AddScoresDto } from './dto/add_scores.dto';
+import { AverageScoreDto } from './dto/average_score.dto';
+import { formatError } from 'graphql';
 
 @Injectable()
 export class AssetService {
@@ -32,11 +34,44 @@ export class AssetService {
 
   async addScores(id: string, scores: AddScoresDto): Promise<AssetModel> {
     await this.assetRepository.update(id, {
+      //   ...(user.name && { name: user.name })
       score_type1: scores.score_type1,
       score_type2: scores.score_type2,
       score_type3: scores.score_type3,
     });
 
     return this.assetRepository.findOneOrFail(id);
+  }
+
+  async getAverageScore(
+    asset_type: string,
+    score_type: string,
+  ): Promise<AverageScoreDto> {
+    let field: string;
+    switch (score_type) {
+      case 'Type1':
+        field = 'score_type1';
+        break;
+
+      case 'Type2':
+        field = 'score_type2';
+        break;
+
+      case 'Type3':
+        field = 'score_type3';
+        break;
+    }
+    let data = await this.assetRepository
+      .createQueryBuilder('assets')
+      .select(['id', 'time_added', field])
+      .where('assets.asset_type = :asset_type', { asset_type })
+      .getRawMany();
+
+    let sum: number = 0;
+    data.forEach((element) => {
+      sum += element[field];
+    });
+
+    return { average_score: sum / data.length }; 
   }
 }
